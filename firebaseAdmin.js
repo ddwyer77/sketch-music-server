c21a36e61e8764ec0453cbf5a356245c41fde103
kegcheck <65734212+kegcheck@users.noreply.github.com>
2025-05-22 21:55:09 -0500

    remove fs

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -3,9 +3,7 @@ import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import fs from 'fs';
 
-const serviceAccount = JSON.parse(
-    fs.readFileSync(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH, 'utf8')
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 if (!getApps().length) {
     initializeApp({ credential: cert(serviceAccount) });
